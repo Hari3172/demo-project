ba78bd9a28728e79120e6dd244b8483320a815b0
Hari3172 <126946415+Hari3172@users.noreply.github.com>
2024-05-01 11:07:37 +0530

    Final Commit of Commits 1,2,3,4

--- demo/src/main/java/com/ty/demo/FeatureDemo.java
+++ demo/src/main/java/com/ty/demo/FeatureDemo.java
@@ -3,5 +3,6 @@ package com.ty.demo;
 public class FeatureDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello World! -> FeatureDemo");
+		System.out.println("Changes for commit 3");
 	}
 }
--- demo/src/main/java/com/ty/demo/Test.java
+++ demo/src/main/java/com/ty/demo/Test.java
@@ -1,7 +1,8 @@
 package com.ty.demo;
 
-public class Demo {
+public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("This file is going to chage and adding commit.");
 	}
 }
